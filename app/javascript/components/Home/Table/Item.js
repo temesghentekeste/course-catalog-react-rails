41c15d380bf2dddd271dd3aa6cbba6535e356257
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-25 10:46:41 +0300

    Basic structure of Item comp.

---
+++ app/javascript/components/Home/Table/Item.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Item() {
+    return (
+        <div className="row">
+            <div className="col-md-10 offset-md-1">
+                <div className="text-ecnter">
+                    <div className="card px-5">
+                        <div className="row">
+                            <div className="col-md-4">
+
+                            </div>
+
+                            <div className="col-md-8">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Item
