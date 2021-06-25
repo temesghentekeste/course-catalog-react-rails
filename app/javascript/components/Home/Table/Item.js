3422e8e82189f99799301c7692117e30d99333d3
Temesghen <mail.temesghen.tekeste@gmail.com>
2021-06-25 12:03:21 +0300

    Add style to cta button inside Item comp.

--- app/javascript/components/Home/Table/Item.js
+++ app/javascript/components/Home/Table/Item.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import styled from 'styled-components'
 import Thumbnail from './Thumbnail';
 
 function Item(props) {
+
+
+const Button = styled.a`
+display: inline-block;
+padding: 10px 20px;
+text-decoraton: none;
+cursor: pointer;
+border-radius: none;
+font-weight: bold;
+background: #fff;
+box-shadow: 0 0 0 3px #473528, -6px 6px #ef5f17, -6px 6px 0 3px #473228;
+color: #888;
+font-size: 18px;
+`
   return (
     <div className="row pt-4 pb-4">
       <div className="col-md-10 offset-md-1">
-        <div className="text-ecnter">
+        <div>
           <div className="card px-5">
             <div className="row">
               <div className="col-md-4">
@@ -16,9 +31,9 @@ function Item(props) {
                   <h4>{props.title}</h4>
                   <p>{props.description}</p>
                   <div className="cta-wrapper">
-                    <a href="#" className="btn cta-btn">
+                    <Button href="#" className="btn cta-btn">
                       Watch This Video
-                    </a>
+                    </Button>
                   </div>
                 </div>
               </div>
